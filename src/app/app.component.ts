3a231a93dea1e406460c0a4bf0981a44fb0b8d8e
khouryGit <suser5412@gmail.com>
2023-09-14 20:42:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageService } from './image/shared/image.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gallery_ng';
+  title = 'Photo Gallery';
 }
